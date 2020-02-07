ffede173dd8814979b382c6218518382cba87d20
wenthomas <wenthomas@163.com>
2020-02-08 01:47:27 +0800

    Scala工程：helloworld

---
+++ my-scala-study/src/main/scala/com/wenthomas/helloworld/HelloWorld.scala
@@ -0,0 +1,11 @@
+package com.wenthomas.helloworld
+
+/**
+ * @author Verno
+ * @create 2020-02-08 1:45 
+ */
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("hello scala")
+  }
+}

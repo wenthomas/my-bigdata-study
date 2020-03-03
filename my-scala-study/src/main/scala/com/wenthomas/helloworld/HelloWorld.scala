50995fe12c7f32ea5885994d295169724b5333e4
wenthomas <wenthomas@163.com>
2020-03-03 22:06:39 +0800

    Scala: 1,模拟map、filter、foreach、reduce操作

--- my-scala-study/src/main/scala/com/wenthomas/helloworld/HelloWorld.scala
+++ my-scala-study/src/main/scala/com/wenthomas/helloworld/HelloWorld.scala
@@ -7,5 +7,16 @@ package com.wenthomas.helloworld
 object HelloWorld {
   def main(args: Array[String]): Unit = {
     println("hello scala")
+    val a = 10;
+    println(a)
+    val str = s"the input is $a"
+    println(str)
+    val row ="""
+        |第一行
+        |第二行
+        |第三行
+        |第四行
+        |""".stripMargin
+    println(row)
   }
 }
---
+++ my-scala-study/src/main/scala/com/wenthomas/helloworld/Test1.scala
@@ -0,0 +1,19 @@
+package com.wenthomas.helloworld
+
+/**
+ * @author Verno
+ * @create 2020-03-02 14:52 
+ */
+object Test1 {
+    def main(args: Array[String]): Unit = {
+        val s =
+            """
+              |dsadsa
+              |""".stripMargin
+        println(s)
+
+        val a = 10 + 20
+        println(a)
+
+    }
+}

49ec599944e9115fdc176d107c8cb20cdb0c32d6
wenthomas <wenthomas@163.com>
2020-03-05 23:55:44 +0800

    Scala: 补充说明

--- my-scala-study/src/main/scala/com/wenthomas/myWhile/Loop.scala
+++ my-scala-study/src/main/scala/com/wenthomas/myWhile/Loop.scala
@@ -8,7 +8,11 @@ import scala.annotation.tailrec
  */
 
 /**
- * 使用scala来模拟Java语言中While循环
+ * 使用scala来模拟Java语言中While循环：通过函数柯里化、高阶函数、控制抽象（名调用）来实现
+ * 名调用：传递整个代码块，而不是传递值
+ *      实现方法：参数列表写成 xxx: => Any
+ *
+ *      xxx: () => Any 这是函数类型
  */
 object Loop {
     def main(args: Array[String]): Unit = {

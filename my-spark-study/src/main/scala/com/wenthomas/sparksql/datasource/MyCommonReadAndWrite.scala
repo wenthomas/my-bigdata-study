689f4cb289cc0043853321385113a51fe8771570
wenthomas <wenthomas@163.com>
2020-03-21 00:00:08 +0800

    spark: spark SQL数据源通用读写， spark SQL通过jdbc读写数据

--- my-spark-study/src/main/scala/com/wenthomas/sparksql/datasource/MyCommonReadAndWrite.scala
+++ my-spark-study/src/main/scala/com/wenthomas/sparksql/datasource/MyCommonReadAndWrite.scala
@@ -1,5 +1,6 @@
 package com.wenthomas.sparksql.datasource
 
+import com.wenthomas.sparksql.function.User2
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.{SaveMode, SparkSession}
 
@@ -11,6 +12,7 @@ import org.apache.spark.sql.{SaveMode, SparkSession}
 /**
  * 通用读写数据源方法：
  *      通用读：spark.read.format("文件格式类型").load(文件路径)
+ *      通用写：df.write.mode("文件保存模式").format("文件输出格式").save(输出路径)
  */
 object MyCommonReadAndWrite {
 
@@ -19,6 +21,7 @@ object MyCommonReadAndWrite {
 
         val spark = SparkSession.builder().config(conf).getOrCreate()
 
+        import spark.implicits._
         //--------------------------------------------------------------------------------------------------------
         //(1) 通用读写json文件
         //      读
@@ -26,7 +29,6 @@ object MyCommonReadAndWrite {
         val df = spark.read.format("json").load("file:///E:/1015/my_data/1.json")
 
         val df1 = spark.read.json("file:///E:/1015/my_data/1.json")
-        df1.show()
 
         //      直接在json文件上执行SQL查询
         spark.sql(
@@ -35,18 +37,20 @@ object MyCommonReadAndWrite {
                 |""".stripMargin).show()
 
         //      写
-
-
+//        df1.write.mode("append").format("json").save("output/")
+//        df1.write.mode("append").json("output/")
 
 
         //--------------------------------------------------------------------------------------------------------
         //(2) 通用读parquet文件（默认）
         //      读
+/*        val parDF = spark.read.parquet("output/user.parquet/part-00000-57dca19e-6cfc-4b2f-844f-7971b7dce51d.snappy.parquet")
+        val userDS = parDF.as[User2]
+*/
+
+
         //      写
-        df.write.mode(SaveMode.Overwrite).parquet("output/user.parquet")
-
-
-
+        //df.write.mode(SaveMode.Overwrite).parquet("output/user.parquet")
 
 
         spark.stop()

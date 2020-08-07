3ee95beed9963561b02a25d18d8699fd82f491e0
wenthomas <wenthomas@163.com>
2020-08-07 17:14:39 +0800

    my-alogorithms-study: 算法题-“好字符串”问题优化

--- my-alogorithms-study/src/main/java/com/wenthomas/alogorithms/NumSplits.java
+++ my-alogorithms-study/src/main/java/com/wenthomas/alogorithms/NumSplits.java
@@ -17,11 +17,11 @@ import java.util.Map;
  */
 public class NumSplits {
     public static void main(String[] args) {
-        System.out.println(numSplits("acbadbaada"));
+        System.out.println(numSplits2("acbadbaada"));
     }
 
     /**
-     * 思路：
+     * 思路：暴力解法（双层for循环）
      * 1，左右两边维护一个哈希表，对字母计数
      * 2，只有当字母的计数从 0 变为 1 时，或者从 1 变为 0 时，字母个数才会变化
      * 3，左右两边字母个数相同时，就是好分割
@@ -57,6 +57,53 @@ public class NumSplits {
             //初始化左右两边子字符串
             left.clear();
             right.clear();
+        }
+
+        return result;
+    }
+
+    /**
+     * 优化算法：滑动窗口（一层for循环）
+     * @param s
+     * @return
+     */
+    public static int numSplits2(String s) {
+        //使用两个HashSet记录左右两个子字符串的不同字母个数
+        Map<Character, Integer> left = new HashMap();
+        Map<Character, Integer> right = new HashMap();
+        int length = s.length();
+        int result = 0;
+
+        //先将全部字符串归为右子字符串
+        for (int i = 0; i < length; i++) {
+            if (right.containsKey(s.charAt(i))) {
+                right.put(s.charAt(i), right.get(s.charAt(i)) + 1);
+            } else {
+                right.put(s.charAt(i), 1);
+            }
+        }
+
+        //从左往右滑动窗口
+        for (int i = 0; i < length; i++) {
+            if (left.containsKey(s.charAt(i))) {
+                left.put(s.charAt(i), left.get(s.charAt(i)) + 1);
+            } else {
+                left.put(s.charAt(i), 1);
+            }
+
+            if (right.containsKey(s.charAt(i))) {
+                if (right.get(s.charAt(i)) > 0) {
+                    right.put(s.charAt(i), right.get(s.charAt(i)) - 1);
+                }
+                if (right.get(s.charAt(i)) == 0) {
+                    right.remove(s.charAt(i));
+                }
+            }
+
+            //判断左右两边子字符串不同字符数是否相等，是则result+1，否则继续
+            if (left.keySet().size() == right.keySet().size()) {
+                result += 1;
+            }
         }
 
         return result;

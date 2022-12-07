029139226bee8b404486195954fbc1511cc92258
wenthomas <wenthomas@163.com>
2022-12-07 19:35:52 +0800

    2022.12.07 my-alogorithms-study: practice

---
+++ my-alogorithms-study/src/main/java/com/wenthomas/alogorithms/TrieNode.java
@@ -0,0 +1,18 @@
+package com.wenthomas.alogorithms;
+
+/**
+ * 前缀树（数据结构：每个节点都有26个子节点的数结构）
+ * @author Verno
+ * @create 2022-12-07 19:03
+ */
+public class TrieNode {
+    //树的末节点
+    boolean val;
+    //每个树都有26个子节点
+    TrieNode[] children;
+
+    public TrieNode() {
+        this.val = false;
+        this.children = new TrieNode[26];
+    }
+}

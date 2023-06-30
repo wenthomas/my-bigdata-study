a84671b1f8db8350506542e0c455b6b7a5839626
wenthomas <wenthomas@163.com>
2023-06-30 17:54:27 +0800

    2023.06.30 my-alogorithms-study:test

---
+++ my-alogorithms-study/src/main/java/com/wenthomas/alogorithms/HelloWorld2.java
@@ -0,0 +1,20 @@
+package com.wenthomas.alogorithms;
+
+import scala.Int;
+
+public class HelloWorld2 {
+
+
+    public static void main(String[] args) {
+        Integer a = 0;
+
+        Integer b = 0;
+        if (a == b) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+        boolean equals = a.equals(b);
+        System.out.println("result: " + equals);
+    }
+}
